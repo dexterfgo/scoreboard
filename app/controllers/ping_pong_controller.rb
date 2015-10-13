d851267981c7c412d36cf3371cfb8a0cc4949043
James Maniscalco <maniscalco.jt@gmail.com>
2015-10-13 11:27:07 -0400

    power rankings

--- app/controllers/ping_pong_controller.rb
+++ app/controllers/ping_pong_controller.rb
@@ -1,7 +1,8 @@
 class PingPongController < ApplicationController
 
   def home
-    @ping_pong_games = PingPongGame.all
+    @ping_pong_games = PingPongGame.last(10).reverse
+    @power_ranks = Player.all.map { |player| [player.name, power_rank(player.id)] }
   end
 
   def new_game
@@ -23,5 +24,56 @@ class PingPongController < ApplicationController
   private
     def game_params
       params.require(:ping_pong_game).permit(:date, :player_one_score, :player_two_score, :player_one_id, :player_two_id)
+    end
+
+    def pvp_rank(player_a_id, player_b_id)
+      @player_a = Player.find(player_a_id)
+      @player_b = Player.find(player_b_id)
+
+      games = (@player_a.ping_pong_games & @player_b.ping_pong_games).uniq
+    
+      if games.count < 5
+        return 0
+      else
+        wins = 0.0
+
+        games.each do |g|
+          if g.player_one_id == @player_a.id and g.player_one_score > g.player_two_score
+            wins += 1
+          elsif g.player_two_id == @player_a.id and g.player_one_score < g.player_two_score
+            wins += 1
+          end
+        end
+
+        win_percentage = wins / games.count
+
+        return win_percentage
+      end
+    end
+
+    def power_rank_chain(players)
+      factor = 1
+      if players.length > 2
+        for i in 0..(players.length - 2)
+          factor = factor * pvp_rank(players[i].id,players[i+1].id)
+        end
+      end
+      puts factor
+      return factor
+    end
+
+    def power_rank(player_id)
+      @player = Player.find(player_id)
+
+      opponents = Player.where.not(id: player_id)
+
+      rank = 0.0
+
+      opponents.permutation.each do |perm|
+        rank = rank + pvp_rank(@player.id, perm[0].id)*power_rank_chain(perm)
+      end
+
+
+      return rank*100
     end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,12 @@
 class Player < ActiveRecord::Base
   validates :name, presence: true
 
-  has_many :ping_pong_games
+  has_many :ping_pong_games_as_player_one, class_name: 'PingPongGame', foreign_key: :player_one_id
+  has_many :ping_pong_games_as_player_two, class_name: 'PingPongGame', foreign_key: :player_two_id
+
+  def ping_pong_games
+    (ping_pong_games_as_player_one.all + ping_pong_games_as_player_two.all).uniq
+  end
 
 end
 
